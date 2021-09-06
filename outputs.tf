36f5bf21e6b2270c391acc567f973e14a965911d
ankitbisht92 <48024427+ankitbisht92@users.noreply.github.com>
2021-09-06 17:04:19 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,18 +3,3 @@ output "cdn_profile_name" {
     value = azurerm_cdn_profile.cdn_profile.name
     description = "CDN Profile Name"
 }
-
-/*output "cdn_endpoint_name" {
-    value = azurerm_cdn_endpoint.cdn_endpoint.name
-    description = "CDN Profile Name"
-}
-
-output "origin_name" {
-    value = azurerm_cdn_endpoint.cdn_endpoint.origin.name
-    description = "Origin Name"
-}
-
-output "origin_host_name" {
-    value = azurerm_cdn_endpoint.cdn_endpoint.origin.host_name
-    description = "Origin Host Name"
-}*/
